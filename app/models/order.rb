eab33e83bababd03676661f004797c28c895154d
秦守丽 <qsl0705@163.com>
2017-05-25 15:19:56 +0800

    config qiniu

--- app/models/order.rb
+++ app/models/order.rb
@@ -48,4 +48,5 @@ class Order < ApplicationRecord
     event :cancel_order do
       transitions from: [:order_placed, :paid], to: :order_cancelled
     end
+  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,9 +7,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
-
+    storage :qiniu
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :medium do
-    process resize_to_fill: [400,400] 
+    process resize_to_fill: [400,400]
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
